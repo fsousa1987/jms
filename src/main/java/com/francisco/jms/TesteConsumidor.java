918174351f5014d1861156f2251f98b4383898da
Francisco Sousa <franciscoeds1987@gmail.com>
2021-03-03 20:23:38 -0300

    Consumindo mensagens com JMS

---
+++ src/main/java/com/francisco/jms/TesteConsumidor.java
@@ -0,0 +1,30 @@
+package com.francisco.jms;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+
+public class TesteConsumidor {
+
+    public static void main(String[] args) throws Exception {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination fila = (Destination) context.lookup("financeiro");
+        MessageConsumer consumer = session.createConsumer(fila);
+
+        Message message = consumer.receive();
+
+        System.out.println("Recebendo msg: " + message);
+
+//		new Scanner(System.in).nextLine();
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
