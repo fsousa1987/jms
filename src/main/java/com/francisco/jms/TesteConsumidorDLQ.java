51bc0db995a3feb35e7197d744cb945119d7b1f6
Francisco Sousa <franciscoeds1987@gmail.com>
2021-03-09 15:48:30 -0300

    Acessando DLQ

---
+++ src/main/java/com/francisco/jms/TesteConsumidorDLQ.java
@@ -0,0 +1,30 @@
+package com.francisco.jms;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import java.util.Scanner;
+
+@SuppressWarnings("DuplicatedCode")
+public class TesteConsumidorDLQ {
+
+    public static void main(String[] args) throws Exception {
+
+        InitialContext context = new InitialContext();
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+        Connection connection = factory.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination fila = (Destination) context.lookup("financeiro");
+        MessageConsumer consumer = session.createConsumer(fila);
+
+        consumer.setMessageListener(System.out::println);
+
+        new Scanner(System.in).nextLine();
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
